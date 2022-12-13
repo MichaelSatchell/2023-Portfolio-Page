b13d3fa57e1e7ec0152c2c650b5a6809a13bc28b
Michael Satchell <michaelsatchelljr@outlook.com>
2022-12-12 17:25:19 -0700

    added a responsive navagation bar for web and cell phone. Also changed the animation rate of child elements when the scroll animation occurs. Lastly, added red square divs as project card placements. Currnetly set in a display flex to align in a row. However, thingking about changing them to a column.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -10,3 +10,12 @@ const observer = new IntersectionObserver((entries) => {
 });
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+const hamburgerBtn = document.getElementsByClassName('hamburger-menu')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+hamburgerBtn.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')})
+
+
