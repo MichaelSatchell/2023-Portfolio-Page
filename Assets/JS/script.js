2a09cfa5c212653c82f6268ed5a4fb3f422266ea
Michael Satchell <michaelsatchelljr@outlook.com>
2022-12-13 07:51:21 -0700

    added a javascript function to slide targeted items from the right into the middle so that on scroll items will appear from the left side first and then the right side next.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -18,4 +18,19 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 hamburgerBtn.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')})
 
+// ...On scroll slide in from the right..
+
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((el) => observer.observe(el));
+
 
